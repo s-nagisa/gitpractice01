094aee0807ce3cf9b4f10ae87dfdb14e464cd723
Nagisa <nagisa.sekiguchi@tcs.com>
2025-06-10 13:30:38 +0900

    3回目

--- GitPractice.java
+++ GitPractice.java
@@ -2,5 +2,6 @@ public class GitPractice{
     public static void main(String[] args) {
         System.out.println("sekiguchiブランチからプッシュ");
         System.out.println("2回目");
+        System.out.println("jikken");
     }
 }
